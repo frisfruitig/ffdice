ab02128a46932736bcd34d9963a403ec523b9411
FrisFruitig <info@frisfruitig.com>
2022-12-16 15:22:45 +0100

    Roll die on joystick input

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ from time import sleep
 die = Die()
 hat = SenseHat()
 
-# Simulate a rolling die by calling the `roll()` function a couple of times.
-# Set its argument `animate` to `True` in order to prevent the same number from being generated twice in a row.
-i = 0
-while (i < 10):
-  number = die.roll(True)
-  hat.set_pixels(matrix[number])
-  # print("Looks like it's going to be %d..." % (number))
-  sleep(0.1)
-  i += 1
+while True:
+  # Roll the die each time the SenseHat joystick is pressed
+  event = hat.stick.wait_for_event()
+  if(event.action == "pressed" and event.direction == "middle"):
+    # Simulate a rolling die by calling the `roll()` function a couple of times.
+    # Set its argument `animate` to `True` in order to prevent the same number from being generated twice in a row.
+    i = 0
+    while (i < 10):
+      number = die.roll(True)
+      hat.set_pixels(matrix[number])
+      # print("Looks like it's going to be %d..." % (number))
+      sleep(0.1)
+      i += 1
 
-# # Use the last output as the definitive value.
-# print("You rolled %d!" % (number))
+    # Log the last output as the definitive value.
+    print("You rolled %d!" % (number))
+    sleep(0.1)
