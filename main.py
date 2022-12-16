09874c7192a19864c084eef14d42bd5e66d3c9ec
FrisFruitig <info@frisfruitig.com>
2022-12-16 14:43:10 +0100

    Add SenseHat implementation
    Disclaimer: this commit, with slight modifications in the Die class, was only tested on https://trinket.io/sense-hat, not with the actual SenseHat yet.

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from inc.Die import Die
 from inc.LED import matrices as matrix
+from sense_hat import SenseHat
 from time import sleep
-
-# print(matrix[1])
 
 # Create a basic die with 6 sides.
 die = Die()
+hat = SenseHat()
 
 # Simulate a rolling die by calling the `roll()` function a couple of times.
 # Set its argument `animate` to `True` in order to prevent the same number from being generated twice in a row.
 i = 0
 while (i < 10):
   number = die.roll(True)
-  print("Looks like it's going to be %d..." % (number))
+  hat.set_pixels(matrix[number])
+  # print("Looks like it's going to be %d..." % (number))
   sleep(0.1)
   i += 1
 
-# Use the last output as the definitive value.
-print("You rolled %d!" % (number))
+# # Use the last output as the definitive value.
+# print("You rolled %d!" % (number))
