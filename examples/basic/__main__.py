ee1d22d5664a304f6f1deb9b5d9ac750c23e1d3f
FrisFruitig <info@frisfruitig.com>
2023-05-11 21:17:03 +0200

    Calculate the sum of two dice

--- examples/basic/__main__.py
+++ examples/basic/__main__.py
@@ -2,8 +2,10 @@
 from src.Die import Die
 
 # By omitting parameters in the instance creation, you'll create a die with 6 sides.
-die = Die()
+die_one = Die()
+die_two = Die()
 
 # For this instance, the `roll()` function will return a number between 1 and 6.
-number = die.roll()
-print("You rolled %d!" % number)
+a = die_one.roll()
+b = die_two.roll()
+print("You rolled %d and %d, adding up to a total of %d!" % (a, b, a + b))
